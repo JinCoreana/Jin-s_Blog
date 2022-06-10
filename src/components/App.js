da8c8ea66bc7ae86628959d48df0f48e3bab304b
Seoyeonjin Choi <itmustbejin@gmail.com>
2022-06-10 17:18:03 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
   <DataProvider>
 <Routes>
-  <Route path="Jin-s_Blog/" element={<Layout/>}>
+  <Route path="/" element={<Layout/>}>
   <Route index element={<Home/>}/>
-  <Route path="Jin-s_Blog/post">
+  <Route path="post">
     <Route index element={
     <NewPost/>}/>
    <Route path=":id" element={
@@ -23,13 +23,13 @@ function App() {
     />}
     />
       
-  <Route path="Jin-s_Blog/edit">
+  <Route path="edit">
     <Route path=':id'
     element={<EditPost />}/>
   </Route>
   </Route>
 
-  <Route path="Jin-s_Blog/about" element={<About/>}/>
+  <Route path="about" element={<About/>}/>
   <Route path="*" element={<Missing/>}/>
 </Route>
 </Routes>
